161d018fdc86ffd60a4baf92eaf6c4682fccb9a5
Пользователь <valetmail@gmail.com>
2019-11-22 20:59:28 +0300

    Added avatars in Posts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,8 +7,8 @@ const MyPosts = () => {
         <div className='posts'>
           My posts:
           <Post text="Post 1"></Post>
-          <Post text="Post 2"></Post>
-          <Post text="Post 3"></Post>
+          <Post text="Post 2" image="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"></Post>
+          <Post text="Post 3" image="https://www.w3schools.com/w3images/avatar6.png"></Post>
           <Post text="Post 4"></Post>
 
         </div>
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,6 +4,7 @@ import classes from './Post.module.css'
 const Post = (props) => {
     return (
           <div className={classes.post}>
+            <img className={classes.avatarImage} src={props.image ? props.image: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"} alt="User avatar"></img>
             {props.text}
           </div>
     )
