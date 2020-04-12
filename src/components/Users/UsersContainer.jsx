529152065c557bb9f094912e9f1af9517ae1c3d1
Николаев Валерий <valetmail@gmail.com>
2020-04-12 18:36:13 +0300

    Move UsersApiContainerLogic into UsersContainer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,7 @@
+import React from 'react'
+import * as Axios from 'axios'
 import { connect } from 'react-redux';
+import Users from './Users'
 import {
   setUsersAC,
   followAC,
@@ -6,7 +9,50 @@ import {
   setUsersCountAC,
   setCurrentPageAC
 } from '../../Redux/Reducers/usersReducer';
-import UsersApiContainer from './UsersApiContainer';
+
+class UsersContainer extends React.Component {
+
+  componentDidMount() {
+    this.setUsers();
+  }
+
+  changePage(pageNumber) {
+    this.setUsers(pageNumber);
+  }
+
+  setUsers(pageNumber){
+    this.fetchUsers(this.props.pageSize, pageNumber || this.props.currentPage)
+      .then(data => {
+        this.props.setUsers(data.items);
+        this.props.setUsersCount(data.totalCount);
+        if (pageNumber){
+          this.props.setCurrentPage(pageNumber);
+        }
+      })  
+  }
+
+  fetchUsers(pageSize, currentPage) {
+    return Axios.get(
+      `https://social-network.samuraijs.com/api/1.0/users?count=${pageSize}&page=${currentPage}`
+    ).then(response => {
+      return response.data;
+    });
+  }
+
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        users={this.props.users}
+        followUser={this.props.followUser}
+        unfollowUser={this.props.unfollowUser}
+        onPageChanged={this.changePage.bind(this)}
+      ></Users>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
@@ -41,6 +87,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersApiContainer);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
